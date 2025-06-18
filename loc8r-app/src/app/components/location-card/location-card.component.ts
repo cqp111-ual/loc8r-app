55bdfc7d89b99a9b1672c646651dffba80adfe4a
Carlos Quesada Pérez <cqp111@inlumine.ual.es>
2025-06-18 02:03:44 +0200

    add date to loc card

--- loc8r-app/src/app/components/location-card/location-card.component.ts
+++ loc8r-app/src/app/components/location-card/location-card.component.ts
@@ -1,17 +1,17 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { LocationModel } from '../../models/location.model';
 import { addIcons } from 'ionicons';
-import { locationOutline } from 'ionicons/icons';
+import { location, calendarOutline } from 'ionicons/icons';
+import { RatingBadgeComponent } from '../rating-badge/rating-badge.component';
 import {
   IonCard,
   IonCardHeader,
   IonCardTitle,
   IonCardSubtitle,
   IonCardContent,
-  IonIcon
+  IonIcon,
 } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-location-card',
   standalone: true,
@@ -24,50 +24,33 @@ import { CommonModule } from '@angular/common';
     IonCardTitle,
     IonCardSubtitle,
     IonCardContent,
-    IonIcon
+    IonIcon,
+    RatingBadgeComponent
   ],
 })
 export class LocationCardComponent implements OnChanges {
   @Input() location!: LocationModel;
 
-  ratingDecimal: string = '0.0';
-  ratingClass: string = 'rating-zero';
   ratingMessage: string = '';
-  reviewsCount: number = 0;
   geolocationUrl: string = '';
 
   constructor() {
-    // Necesario para que <ion-icon name="location-outline"> funcione
-    addIcons({ locationOutline });
+    addIcons({ location, calendarOutline });
   }
 
   ngOnChanges() {
     const rating = this.location.rating ?? 0;
-    this.ratingDecimal = rating.toFixed(1);
 
     if (rating === 0) {
-      this.ratingClass = 'rating-zero';
       this.ratingMessage = 'Sin valoración';
     } else if (rating < 2.5) {
-      this.ratingClass = 'rating-bad';
       this.ratingMessage = 'Malo';
     } else if (rating < 4) {
-      this.ratingClass = 'rating-average';
       this.ratingMessage = 'Regular';
     } else {
-      this.ratingClass = 'rating-good';
       this.ratingMessage = 'Excelente';
     }
 
-    this.reviewsCount = this.location.numReviews ?? 0;
     this.geolocationUrl = `https://www.google.com/maps?q=${this.location.coordinates[0]},${this.location.coordinates[1]}`;
-  }
-
-  getImageUrl(imageId: string | null): string {
-    if (imageId) {
-      // Aquí puedes poner la URL real que genera la imagen a partir del imageId
-      return `https://mi-servidor-de-imagenes.com/images/${imageId}.jpg`;
-    }
-    return 'https://ionicframework.com/docs/img/demos/card-media.png';
   }
 }
